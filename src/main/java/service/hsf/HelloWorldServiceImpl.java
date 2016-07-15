2998504c71026ab3b5ffcc114c81cb00e0530349
tianyang <guogen.lgg@alibaba-inc.com>
2016-07-15 15:27:37 +0800

    并发用例

--- src/main/java/service/hsf/HelloWorldServiceImpl.java
+++ src/main/java/service/hsf/HelloWorldServiceImpl.java
@@ -1,8 +1,11 @@
 package service.hsf;
 
+import org.springframework.stereotype.Service;
+
 /**
  * Created by guogen.lgg on 2016/7/13.
  */
+@Service("HelloWorldServiceImpl")
 public class HelloWorldServiceImpl implements HelloWorldService {
     public String sayHello(String name) {
         return "hello : " + name;
